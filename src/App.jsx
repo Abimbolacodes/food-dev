aba1ab82c16519287f39e847e45f649795cb5465
abimbolacodes <abimbolalatunji>
2025-01-27 17:40:01 +0100

    samuel is a boy

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import Navbar from './Components/Navbar/Navbar'
 import { Route, Routes } from 'react-router-dom'  
 import Cart from './Pages/Cart/Cart'
 import PlaceOrder from './Pages/PlaceOrder/PlaceOrder'
-import Home from './Pages/Home/home'
 import Footer from './Components/Footer/footer'
 import { useState } from 'react'
 import LoginPopup from './Components/loginPopup/loginPopUp'
+import Home from './Pages/Home/home'
 
 const App = () => {
 
