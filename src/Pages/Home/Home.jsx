73f8c62719921e514162bcd793b449ba5a1e95b9
Abimbola Olatunji <161823517+Abimbolacodes@users.noreply.github.com>
2025-06-27 17:58:16 +0100

    Fix import path for Header component in Home.jsx

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import "./Home.css"
-import Header from '../../Components/header/Header'
+import Header from '../../Components/Header/Header'
 import ExploreMenu from '../../Components/ExploreMenu/ExploreMenu'
 import FoodDisplay from '../../Components/FoodDisplay/FoodDisplay'
 import AppDownload from '../../Components/AppDownload/AppDownload'
